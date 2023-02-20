31f622e629cb844eefd6a962fa62a62a9acf93a4
dodofit <fittondorian@gmail.com>
2023-02-20 22:54:00 +0100

    preds 2021

--- epftoolbox/data/__init__.py
+++ epftoolbox/data/__init__.py
@@ -1,4 +1,3 @@
 from ._wrangling import (scaling, DataScaler)
-from ._datasets import (read_data)
-from ._datasets import (read_data_refreshed)
+from ._datasets import (read_data, read_local_data, read_data_refreshed)
 from ._datasets_sokhna import (read_data_refreshed_2)
